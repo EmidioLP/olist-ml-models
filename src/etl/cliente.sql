a072503989fff406a99ba789e14183c1f579bfa4
Emidio <emidio551@gmail.com>
2023-04-06 14:44:59 -0300

    Join para calcular estados de venda

--- src/etl/cliente.sql
+++ src/etl/cliente.sql
@@ -1,17 +1,22 @@
-SELECT DISTINCT
+WITH tb_join AS(
+
+    SELECT DISTINCT
         t1.idPedido,
         t1.idCliente,
         t2.idVendedor,
         t3.descUF
 
-FROM pedido AS t1
+    FROM pedido AS t1
 
-LEFT JOIN item_pedido as t2
-ON t1.idPedido = t2.idPedido
+    LEFT JOIN item_pedido as t2
+    ON t1.idPedido = t2.idPedido
 
-LEFT JOIN cliente as t3
-ON t1.idCliente = t3.idCliente
+    LEFT JOIN cliente as t3
+    ON t1.idCliente = t3.idCliente
 
-WHERE t1.dtPedido < '2018-01-01'
-AND dtPedido >= julianday('2018-01-01', '-6 months')
-AND idVendedor IS NOT NULL
+    WHERE t1.dtPedido < '2018-01-01'
+    AND dtPedido >= julianday('2018-01-01', '-6 months')
+    AND idVendedor IS NOT NULL
+)
+
+SELECT * FROM tb_join
